a429f8e6e0d8a5dc4b8f2365c293f90e5d28e0fa
phanvietdung2101 <phanvietdung2101@gmail.com>
2020-08-01 10:13:47 +0700

    done router style

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
+    <div id="navbar">
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+      >
+        <el-row type="flex" justify="center">
+          <el-col :span="3">
+            <el-menu-item index="1">
+              <router-link to="/">Home</router-link>
+            </el-menu-item>
+          </el-col>
+          <el-col :span="3">
+            <el-menu-item index="2">
+              <router-link to="/JokeList">JokeList</router-link>
+            </el-menu-item>
+          </el-col>
+        </el-row>
+      </el-menu>
+    </div>
+    <div id="content">
+      <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  components: {
-  }
-}
+  name: "app",
+  components: {},
+  data() {
+    return {
+      activeIndex: "1",
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
