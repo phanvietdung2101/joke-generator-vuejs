07b5069dc0b70f4bcbcbf200f3c577636f4ae437
phanvietdung2101 <phanvietdung2101@gmail.com>
2020-08-01 09:57:59 +0700

    add element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link>  ||
-    <router-link to="/JokeList">Joke List</router-link>
-    <router-view></router-view>
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
   </div>
 </template>
 
 <script>
-// import Home from '@/views/Home'
-// import JokeList from '@/views/JokeList'
+
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    // Home,
-    // JokeList
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
